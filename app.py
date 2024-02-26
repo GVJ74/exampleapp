c9418d510e9daf594784a28690ee6522b646668f
TheEarlOfGray <80707106+TheEarlOfGray@users.noreply.github.com>
2024-02-26 15:42:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return 'THere is some updated text'
+    return 'Here is some updated text'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
